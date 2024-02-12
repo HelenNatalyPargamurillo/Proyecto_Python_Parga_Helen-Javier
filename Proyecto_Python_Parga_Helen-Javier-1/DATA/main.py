58e0589769642f0317d71464116029cac78a7aa7
HelenNatalyPargamurillo <156430545+HelenNatalyPargamurillo@users.noreply.github.com>
2024-02-12 09:20:36 -0500

    Add files via upload

---
+++ Proyecto_Python_Parga_Helen-Javier-1/DATA/main.py
@@ -0,0 +1,56 @@
+print("-------------------")
+print("----Bienvenido a Campusland----")
+print("-------------------")
+cond=True
+while cond == True:
+    print("-------------------")
+    print("1. trainers")
+    print("2. campers ")
+    print("3. cordinador ")
+    print("4. salir ")
+
+    decision = int (input(""))
+    if  decision ==1:
+        print("-------------------")
+        print("1. ver horario")
+        print("2. ver sala")
+        print("3. salir ")
+        print("-------------------")
+        quehago=int(input("ingresa"))
+        print("")
+        if (quehago== 2):
+            print("si funciono")
+
+    
+    
+    if  decision ==2:
+        print("-------------------")
+        print("1. salir")
+        print("-------------------")
+        
+        quehago=int(input("ingresa"))
+        print("")   
+    
+    if (decision== 1):
+            break
+
+    
+    if  decision ==3:
+        print("-------------------")
+        print("1. RESGISTRAR CAMPER")
+        print("2. NOTAS CAMPERS")
+        print("3. ESTADO CAMPER")
+        print("4. SALAS")
+        print("5. SALIR")
+        quehago1=int(input("ingresa"))
+        
+        if (quehago1== 1):
+            nombre = str(input("nombre camper"))
+            apellido=str(input("iapellidos camper"))
+            direccion=str(input("direcccion"))
+            acudiente=input("acudiente")
+            telefono =input("telefono")
+            estato=input("estato")
+        usu
+        
+        
