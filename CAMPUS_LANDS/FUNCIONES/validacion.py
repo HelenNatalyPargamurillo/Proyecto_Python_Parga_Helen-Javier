bcd095cf87363233106c1c684e3004d932f0d4b7
HelenNatalyPargamurillo <156430545+HelenNatalyPargamurillo@users.noreply.github.com>
2024-02-13 18:27:37 -0500

    avance_proyecto4

---
+++ CAMPUS_LANDS/FUNCIONES/validacion.py
@@ -0,0 +1,28 @@
+
+def eleccion():
+    roles=True
+    while roles :
+        try:
+            cordinacion = int (input("digite una opcion correcta"))
+            while not(1<=cordinacion <= 4):
+                print ("por favor digite una opcion valida")    
+                cordinacion = int (input("digite una opcion correcta"))
+        except ValueError:
+            print("solo valores numericos")
+        else :
+            roles=False
+    return cordinacion
+
+def op_cord():
+    roles=True
+    while roles :
+        try:
+            cordinacion = int (input("digite una opcion correcta"))
+            while not(1<=cordinacion <= 10):
+                print ("por favor digite una opcion valida")    
+                cordinacion = int (input("digite una opcion correcta"))
+        except ValueError:
+            print("solo valores numericos")
+        else :
+            roles=False
+    return cordinacion
